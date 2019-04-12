820e44ae4cd4c7c46a14ff55dadc3529f081130e
Виктор <@SL-SERVISA>
2019-04-12 15:11:55 +0300

    Сделал задание 1, 2 в одном потоке

---
+++ AsyncThreadApplication/ParseTxtToDat.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncThreadApplication
+{
+    class Operation
+    {
+        public float Result { get; set; }
+        public float Number1 { get; set; }
+        public float Number2 { get; set; }
+        public string Operator { get; set; }
+        public bool Fault { get; set; }
+
+        public Operation(string line)
+        {
+            line += " ";
+            bool noFault = false;
+            int num = 0;
+            string temp = null;
+            int operation = 0;
+            float number1 = 0;
+            float number2 = 0;
+            foreach (char c in line)
+            {
+                if (c == ' ' || c == '\n')
+                {
+                    if (num == 0) noFault = int.TryParse(temp, out operation);
+                    else if (num == 1) float.TryParse(temp, out number1);
+                    else float.TryParse(temp, out number2);
+                    temp = null;
+                    num++;
+                }
+                if (!noFault) operation = 0;
+                temp += c;
+            }
+            if (operation == 1)
+            {
+                Result = number1 * number2;
+                Operator = "*";
+            }
+            else if (operation == 2 && number2 != 0)
+            {
+                Result = number1 / number2;
+                Operator = "/";
+            }
+            else Fault = true;
+            Number1 = number1;
+            Number2 = number2;
+        }
+    }
+
+    class ParseTxtToDat
+    {
+
+        /// <summary>
+        /// Читает файл txt
+        /// </summary>
+        public static List<Operation> Read(string path)
+        {
+            List<Operation> results = new List<Operation>();
+            StreamReader sr = new StreamReader(path, Encoding.GetEncoding(1251));
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    Operation parse = new Operation(sr.ReadLine());
+                    if(!parse.Fault) results.Add(parse);
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+            }
+            sr.Close();
+            return results;
+        }
+
+        /// <summary>
+        /// Сохраняет в файл result.dat
+        /// </summary>
+        public static void Save(List<Operation> data, bool add)
+        {
+            if (data == null) return;
+            using (StreamWriter sw = new StreamWriter("result.dat", add))
+            {
+                foreach (var v in data)
+                    sw.WriteLine($"{v.Number1,2} {v.Operator,2} {v.Number2,2}  =  {v.Result}");
+            }
+            data.Clear();
+        }
+    }
+}
---
+++ AsyncThreadApplication/Program.cs
@@ -0,0 +1,100 @@
+﻿// Чернышов Виктор. Урок 6
+
+/* Задание 1:
+ * Даны две двумерных матрицы размерностью 100х100 каждая.
+ * Написать приложение, производящее их параллельное умножение.
+ * Матрицы заполняются случайными целыми числами от 0 до 10. */
+
+/* Задание 2:
+ * В директории лежат файлы. По структуре они содержат три числа,
+ * разделенные пробелами. Первое число — целое, обозначает действие: 
+ * 1 — умножение и 2 — деление. Остальные два — числа с плавающей точкой.
+ * Написать многопоточное приложение, выполняющее эти действия над числами
+ * и сохраняющее результат в файл result.dat . Файлов в директории
+ * заведомо много. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncThreadApplication
+{
+    class Program
+    {
+        static int size = 100;
+        static int[,] Create()
+        {
+            Random rnd = new Random();
+            int[,] matrix = new int[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = rnd.Next(10);
+                    Console.Write(matrix[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+            return matrix;
+        }
+        static int[,] Multiplication(int[,] a, int[,] b)
+        {
+            int[,] r = new int[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    for (int k = 0; k < size; k++)
+                    {
+                        r[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            return r;
+        }
+        static void Print(int[,] a)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            #region Задание 1
+            Console.WriteLine("Первая матрица:");
+            int[,] A = Create();
+            Console.WriteLine("Вторая матрица:");
+            int[,] B = Create();
+            Console.WriteLine("Результирующая матрица:");
+            int[,] C = Multiplication(A,B);
+            Print(C);
+            #endregion
+
+            #region Задание 2
+            List<string> path = new List<string>()
+            {
+                "..\\..\\data1.txt",
+                "..\\..\\data2.txt",
+                "..\\..\\data3.txt",
+            };
+            List<Operation> results = new List<Operation>();
+            bool add = false;
+            foreach (string file in path)
+            {
+                results= ParseTxtToDat.Read(file);
+                ParseTxtToDat.Save(results, add);
+                add = true;
+            }
+            #endregion
+            Console.WriteLine("Конец программы");
+            Console.ReadKey();
+        }
+    }
+}
